4ee88f937a7da5d84bda3b5a10c7970ca76a8b14
Michal Matlega <m1sz3lek@gmail.com>
2017-03-02 18:27:56 +0100

    Coding

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var cheerio = require('cheerio');
+var http = require('http');
+
+var games = [
+    'shenzhen-io',
+    'undertale',
+    'doom'
+]
 
 
 /* GET home page. */
@@ -9,8 +17,70 @@ router.get('/', function(req, res, next) {
  	
 });
 
-router.get('/hello', function(req, res, next) {
-  res.render('hello');
+router.get('/games', function(req, res, next) {
+  var games = getPagesArray();
+
+    Promise.all(
+        games.map(loadPage)
+    ).then(function(pages){
+        return Promise.all(pages.map(getBestPrice))
+    }).then(function(render){
+
+        renderr = render.reduce(function(renderr,i){
+            return renderr + i;
+        },"")
+
+        res.send(renderr);
+    });
 });
+
+var loadPage = function(options){
+    return new Promise(function(resolve, reject){
+            http.get(options, function(http_res) {
+                var data = "";
+
+                http_res.on("data", function (chunk) {
+                    data += chunk;
+                });
+
+                http_res.on("end", function () {
+                    resolve(data);
+                });
+        }).on('error', function(e) {
+            reject("Got error: " + e.message);
+        });
+    });
+    
+}
+
+var getBestPrice = function(data){
+    
+    return new Promise(function(resolve){
+        var render = "";
+
+        var $ = cheerio.load(data);
+                
+        render += $('.game-title').text() + " ";
+        render += $('.local-price-column').eq(1).text();
+        render += ' <a href="https://salenauts.com' + $('.price-button').eq(2).attr('href') + '">link</a></br>';
+
+        resolve(render);
+
+    });  
+}
+
+var getPagesArray = function(){
+    var result;
+
+    result = games.map(function(item){
+        var row = [];
+        row['host'] = 'salenauts.com';
+        row['port'] = 80;
+        row['path'] = '/pl/game/' + item +'/';
+        return row;
+    });
+
+    return result;
+}
 
 module.exports = router;
