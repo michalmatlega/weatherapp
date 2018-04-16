f22c52be8236e0a355ffa025a52f4a501ecc9130
Michal Matlega <m1sz3lek@gmail.com>
2018-04-16 21:38:09 +0200

    Env file!

--- playground/records.js
+++ playground/records.js
@@ -1,24 +1,57 @@
 let mongoose = require('mongoose');
 let moment = require('moment');
+require('dotenv').config();
 
 
-// mongoose.connect('mongodb://heroku_n0ph7jfg:mr65n0aancsfng9gkl2qkc3ehp@ds119220.mlab.com:19220/heroku_n0ph7jfg');
+mongoose.connect(process.env.MONGODB_URI);
 //
-// let DayRecord = mongoose.model('DayRecord', {
-//    type: String,
-//    value: String,
-//    date: Date
-// });
+let Weather = mongoose.model('Weather',{
+    latitude: Number,
+    longitude: Number,
+    timezone: String,
+    offset: Number,
+    currently: {
+        time: Number,
+        summary: String,
+        icon: String,
+        precipIntensity: Number,
+        precipProbability: Number,
+        precipType: String,
+        temperature: Number,
+        apparentTemperature: Number,
+        dewPoint: Number,
+        humidity: Number,
+        windSpeed: Number,
+        windBearing: Number,
+        cloudCover: Number,
+        pressure: Number,
+        ozone: Number
+
+        }
+    }, 'weather'
+);
+
+let DayRecord = mongoose.model('DayRecord', {
+   type: String,
+   value: Number,
+   time: String,
+    day: String
+});
 //
-// var dayRecord = new DayRecord({
-//    type: 'temperature',
+// let dayRecord = new DayRecord({
+//     type: 'temperature',
 //     value: 30,
-//     day: 99999
+//
+//
 //
 // });
-
+//
 // dayRecord.save((err) => {
-//     if (err) mongoose.disconnect();
+//     if (err){
+//         console.log(err);
+//         mongoose.disconnect();
+//     }
+//     console.log('saved!');
 //     mongoose.disconnect();
 // });
 
@@ -27,7 +60,49 @@ let moment = require('moment');
 let getMaxTempForDay = function(day){       //example 2018
     let start = moment(day).unix();
     let end = moment(day).add(1, 'days').unix();
-    console.log(start,end);
+
+    Weather.findOne({})
+        .where('currently.time')
+        .gt(start)
+        .lt(end)
+        .sort({'currently.temperature': -1})
+        .exec((err, value) => {
+            if (err) console.log(err);
+            //console.log(value);
+
+          //  if(value.length()){
+                let dayRecord = new DayRecord({
+                    type: 'minTemperature',
+                    value: value.currently.temperature,
+                    time: value.currently.time,
+                    day: day
+                });
+
+                dayRecord.save().then(() => {
+                    console.log(`Max temperature day ${day} saved.`)
+                    mongoose.disconnect();
+                }).catch((err) => {
+                    console.log(err);
+                    mongoose.disconnect();
+                });
+           // }
+
+
+
+
+        });
+
+    //console.log(start,end);
 }
+
+// let daysOfYear = [];
+// let start = moment('2017-01-01');
+// for (let d = start; d.isBefore(); d + d.add(1, 'days')) {
+//     let tmp = moment(d);
+//     daysOfYear.push(tmp);
+// }
+//
+// console.log(daysOfYear);
+
 
 getMaxTempForDay('2018-04-01');
